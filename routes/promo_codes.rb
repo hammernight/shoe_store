4c7ba17a2f5a3e60a9918e8508ca4665d566212e
Oscar Rieken <oscar@ionicsecurity.com>
2014-09-15 20:14:44 -0400

    updated flash class

--- routes/promo_codes.rb
+++ routes/promo_codes.rb
@@ -2,7 +2,7 @@ class ShoeStore < Sinatra::Application
 
   post '/promo' do
     if params[:promo_code].nil? or params[:promo_code].eql? ''
-      flash[:notice] = 'Please enter a promotional code'
+      flash[:alert_danger] = 'Please enter a promotional code'
     elsif params[:promo_code].empty?
       achieve! UsedPromocode
       @promo_code_message = PromoCode.promo_code_message
@@ -10,7 +10,7 @@ class ShoeStore < Sinatra::Application
     elsif params[:promo_code].include? "'"
       achieve! PromoCodeSqlInjection
       haml :error
-    elsif params[:promo_code].include? "<"
+    elsif params[:promo_code].include? '<'
       achieve! PromoCodeCrossSiteScripting
       haml :error
     elsif params[:promo_code].chars.to_a.size > 255
@@ -21,7 +21,7 @@ class ShoeStore < Sinatra::Application
       haml :error
     else
       # TODO do something with this email address
-      flash[:notice] = "The code: #{params[:promo_code]} is not a valid promotional code"
+      flash[:warn] = "The code: #{params[:promo_code]} is not a valid promotional code"
     end
     redirect request.referrer
   end
