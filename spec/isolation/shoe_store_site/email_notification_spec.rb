3eea9570a2cc9237c248807d3505f942cf7d544d
Oscar Rieken <oscar_rieken@homedepot.com>
2016-04-27 02:01:59 -0400

    updated email examples to new rspec syntax :white_check_mark:

--- spec/isolation/shoe_store_site/email_notification_spec.rb
+++ spec/isolation/shoe_store_site/email_notification_spec.rb
@@ -3,23 +3,23 @@ require "spec_helper"
 describe "Email submission" do
   context "Valid email address" do
 
-    let(:subject) { EmailNotification.new("test.user-bob@manheim.com")}
+    let(:subject) { EmailNotification.new('test.user-bob@manheim.com')}
 
     it 'should be a valid email address' do
-      subject.valid?.should be_true
+      expect(subject.valid?).to be_truthy
     end
 
     it 'should be a valid email format' do
-      subject.email_format?.should be_true
+      expect(subject.email_format?).to be_truthy
     end
 
     it 'should have a domain' do
-      subject.missing_domain?.should be_false
+      expect(subject.missing_domain?).to be_falsey
     end
 
     it 'should not cannot contain special characters other than @ . - _' do
-      subject = EmailNotification.new("invalid@email.com")
-      subject.illegal_characters?.should be_false
+      subject = EmailNotification.new('invalid@email.com')
+      expect(subject.illegal_characters?).to be_falsey
     end
 
     it 'email validation should be :valid' do
@@ -27,26 +27,26 @@ describe "Email submission" do
     end
   end
 
-  context "Invalid email address" do
+  context 'Invalid email address' do
 
     it 'email address must be in a valid email format' do
-      subject = EmailNotification.new("not.an.email")
-      subject.valid?.should be_false
+      subject = EmailNotification.new('not.an.email')
+      expect(subject.valid?).to be_falsey
     end
 
     it 'email address cannot contain special characters other than @ . - _' do
-      subject = EmailNotification.new("invalid$@email.com")
-      subject.illegal_characters?.should be_true
+      subject = EmailNotification.new('invalid$@email.com')
+      expect(subject.illegal_characters?).to be_truthy
     end
 
     it 'invalid email format should be :invalid_email' do
-      subject = EmailNotification.new("not.an.email")
-      subject.validate_email.should be :invalid_email
+      subject = EmailNotification.new('not.an.email')
+      expect(subject.validate_email).to eql :invalid_email
     end
 
     it 'email missing domain should be :missing_domain' do
-      subject = EmailNotification.new("test@")
-      subject.validate_email.should be :missing_domain
+      subject = EmailNotification.new('test@')
+      expect(subject.validate_email).to eql :missing_domain
     end
   end
 
