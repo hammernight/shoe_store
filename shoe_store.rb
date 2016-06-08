a1f9728bbc9a150f72383db5272080f3ff67d70c
Oscar Rieken <oscar.rieken@adp.com>
2016-06-08 13:17:34 -0400

    :boom: got dbconfig working, changed tags on admin stats

--- shoe_store.rb
+++ shoe_store.rb
@@ -23,73 +23,54 @@ require 'json'
 
 class ShoeStore < Sinatra::Application
 
-	configure do
-		set :haml, { :format => :html5 }
+  configure do
+    set :haml, {:format => :html5}
     set :protection, :except => [:json_csrf]
-		enable :sessions
+    enable :sessions
 
     set :database_file, './config/database.yml'
-	end
+  end
 
-	# configure :development, :test do
-	# 	ActiveRecord::Base.establish_connection(
-	# 			:adapter => 'sqlite3',
-	# 			:database => 'shoes.db'
-	# 	)
-	# end
-  #
-	# configure :production do
-	# 	db = URI.parse(ENV['DATABASE_URL'] || 'postgresql://localhost/shoe_store')
-  #
-	# 	ActiveRecord::Base.establish_connection(
-	# 			:adapter => 'postgresql',
-	# 			:host => db.host,
-	# 			:username => db.user,
-	# 			:password => db.password,
-	# 			:database => db.path[1..-1],
-	# 			:encoding => 'utf8'
-	# 	)
-	# end
 end
 
 before do
-	unless request.path_info.match /^\/(admin|stylesheet\.css)/
+  unless request.path_info.match /^\/(admin|stylesheet\.css)/
 
-		previous = Request.order(:created_at).last
-		@request_model = Request.create! :data => request.to_yaml, :ip_address => (request.env["HTTP_X_REAL_IP"] || request.ip)
+    previous = Request.order(:created_at).last
+    @request_model = Request.create! :data => request.to_yaml, :ip_address => (request.env["HTTP_X_REAL_IP"] || request.ip)
 
-		if previous && previous.ip_address != (request.env["HTTP_X_REAL_IP"] || request.ip)
-			achieve! MultipleIpAddresses
-		end
+    if previous && previous.ip_address != (request.env["HTTP_X_REAL_IP"] || request.ip)
+      achieve! MultipleIpAddresses
+    end
 
-		Achievement.user_agent_achievements(UserAgent::ParsedUserAgent.new(request.user_agent || ""), @request_model)
-	end
-	@month_names = Date::MONTHNAMES.compact
+    Achievement.user_agent_achievements(UserAgent::ParsedUserAgent.new(request.user_agent || ""), @request_model)
+  end
+  @month_names = Date::MONTHNAMES.compact
 end
 
 get '/' do
-	@title = 'Welcome to the Shoe Store'
-	@brand_names = Brand.all
-	haml :index
+  @title = 'Welcome to the Shoe Store'
+  @brand_names = Brand.all
+  haml :index
 end
 
 get '/stylesheet.css' do
-	content_type 'text/css', :charset => 'utf-8'
-	sass :stylesheet
+  content_type 'text/css', :charset => 'utf-8'
+  sass :stylesheet
 end
 
 helpers do
-	def to_id(name)
-		name.gsub(' ', '').underscore.downcase
-	end
+  def to_id(name)
+    name.gsub(' ', '').underscore.downcase
+  end
 end
 
 private
 
 def achieve!(key)
-	Achievement.achieve! key, @request_model
+  Achievement.achieve! key, @request_model
 end
 
 def sort_shoes!
-	@shoes.to_a.sort! { |s1, s2| Date::MONTHNAMES.index(s1.release_month) <=> Date::MONTHNAMES.index(s2.release_month) }
+  @shoes.to_a.sort! { |s1, s2| Date::MONTHNAMES.index(s1.release_month) <=> Date::MONTHNAMES.index(s2.release_month) }
 end
